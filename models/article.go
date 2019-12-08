6846cbee925f0a7a072545294708f8579c0c1777
l7e <7740280@qq.com>
2019-12-08 22:46:44 +0800

    完成文章添加和单个文章的获取

--- models/article.go
+++ models/article.go
@@ -28,3 +28,52 @@ func (article *Article) BeforeUpdate(scope *gorm.Scope) error {
 	scope.SetColumn("ModifiedOn", time.Now().Unix())
 	return nil
 }
+
+func ExistArticleByID(id int) bool {
+	var article Article
+	db.Where("id = ?", id).First(&article)
+	if article.ID > 0 {
+		return true
+	}
+	return false
+}
+
+func GetArticleTotal(maps interface{}) (count int) {
+	db.Model(&Article{}).Where(maps).Count(&count)
+	return count
+}
+
+func GetArticles(pageNum, pageSize int, maps interface{}) (articles []Article) {
+	db.Preload("Tag").Where(maps).Offset(pageNum).Limit(pageSize).Find(&articles)
+	return articles
+}
+
+func GetArticle(id int) (article Article) {
+	db.First(&article, id)
+	db.Model(&article).Related(&article.Tag)
+	return article
+}
+
+func UpdateArticle(id int, data interface{}) bool {
+	db.Model(&Article{}).Where("id = ?", id).Updates(data)
+	return true
+}
+
+func AddArticle(data map[string]interface{}) bool {
+	db.Create(&Article{
+		TagID:     data["tag_id"].(int),
+		Title:     data["title"].(string),
+		Desc:      data["desc"].(string),
+		Content:   data["content"].(string),
+		CreatedBy: data["created_by"].(string),
+		State:     data["state"].(int),
+	})
+
+	return true
+}
+
+func DeleteArticle(id int) bool {
+	db.Where("id = ?",id).Delete(Article{})
+	return true
+}
+
--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -1,20 +1,98 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin_demo/models"
+	"gin_demo/pkg/e"
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
+)
 
-func GetArticles(c *gin.Context)  {
+func GetArticles(c *gin.Context) {
 
 }
 
-func GetArticle(c *gin.Context)  {
+func GetArticle(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
 
+	valid := validation.Validation{}
+
+	valid.Required(id, "id").Message("id不能为空")
+	valid.Min(id, 1, "id").Message("id必须大于0")
+
+	code := e.INVALID_PARAMS
+
+	var data interface{}
+
+	if !valid.HasErrors() {
+		if models.ExistArticleByID(id) {
+			data = models.GetArticle(id)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Printf("%s", err)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
-func AddArticle(c *gin.Context)  {
+func AddArticle(c *gin.Context) {
+	tagId, _ := strconv.Atoi(c.Query("tag_id"))
+	title := c.Query("title")
+	desc := c.Query("desc")
+	content := c.Query("content")
+	createdBy := c.Query("created_by")
+	state, _ := strconv.Atoi(c.DefaultQuery("state", "0"))
 
+	valid := validation.Validation{}
+	valid.Min(tagId, 1, "tag_id").Message("tag_id不能小于1")
+	valid.Required(title, "title").Message("文章标题不能为空")
+	valid.Required(desc, "desc").Message("简述不能为空")
+	valid.Required(content, "content").Message("内容不能为空")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.Required(state, "state").Message("状态不能为空")
+
+	code := e.INVALID_PARAMS
+
+	if !valid.HasErrors() {
+		if models.ExistTagByID(tagId) {
+			data := make(map[string]interface{})
+			data["tag_id"] = tagId
+			data["title"] = title
+			data["desc"] = desc
+			data["content"] = content
+			data["created_by"] = createdBy
+			data["state"] = state
+
+			models.AddArticle(data)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Printf("err.key : %s ,err.message : %s\n", err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]interface{}),
+	})
 }
 
-func UpdateArticle(cc *gin.Context)  {
+func UpdateArticle(c *gin.Context) {
 
 }
 
